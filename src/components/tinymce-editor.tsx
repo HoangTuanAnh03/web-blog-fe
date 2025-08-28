05ec17d83b7eac3eaa87855e0e6910e4fe95f0a5
Tuấn Anh <138606608+HoangTuanAnh03@users.noreply.github.com>
2025-08-28 21:28:42 +0700

    Update tinymce-editor.tsx

--- src/components/tinymce-editor.tsx
+++ src/components/tinymce-editor.tsx
@@ -34,7 +34,7 @@ export default function TinyMCEEditor({
 
         <div className={!isEditorLoaded ? "hidden" : ""}>
           <Editor
-              apiKey="c7g3bowjbi9v4vokvur1e5uskxie7ky57ykmuzyouehu6hfh"
+              apiKey="rvpeg3by0cajknhv2anwsejeoac49m7kpp2u6dg3b5q0977l"
               onInit={(_: any, editor: any) => {
                 editorRef.current = editor
               }}
